2727ac1ece8d9fe98932d1fd9541425b182f311c
Arthlight <arthurpbh@gmail.com>
2020-08-10 19:11:11 +0200

    fix: updated container name

--- Flat_Scraper_Scrapy/Flat_Scraper_Scrapy/main.py
+++ Flat_Scraper_Scrapy/Flat_Scraper_Scrapy/main.py
@@ -17,14 +17,14 @@ TIMEOUT = 60
 while True:
     sleep(TIMEOUT * 20160)  # sleep for 2 weeks
     try:
-        requests.post('http://hotspothousing:8080/api/before/')
+        requests.post('http://HotspotHousing:8080/api/before/')
     except Exception as error:
         print(error)
     else:
         command = 'scrapy crawl ImmobilienScout'
         subprocess.run(command, shell=True)
         try:
-            requests.post('http://hotspothousing:8080/api/after/')
+            requests.post('http://HotspotHousing:8080/api/after/')
         except Exception as error:
             print(error)
 
